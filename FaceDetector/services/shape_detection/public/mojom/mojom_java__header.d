b38ff1ab77b348493c61dcefec02200e909c2606
RockNRoll <RockNRoll@RockStar.muzik>
2021-10-06 14:07:19 +0900

    add

---
+++ FaceDetector/services/shape_detection/public/mojom/mojom_java__header.d
@@ -0,0 +1,13 @@
+obj/services/shape_detection/public/mojom/mojom_java.turbine.jar: \
+ obj/third_party/android_sdk/android.ijar.jar \
+ ../../third_party/jdk/extras/java_8/jre/lib/rt.jar \
+ obj/base/base_java.turbine.jar \
+ obj/base/jni_java.turbine.jar \
+ obj/build/android/build_config_java.turbine.jar \
+ obj/build/android/native_libraries_java.turbine.jar \
+ obj/mojo/public/java/bindings_java.turbine.jar \
+ obj/mojo/public/java/system_java.turbine.jar \
+ obj/skia/public/mojom/mojom_java.turbine.jar \
+ obj/third_party/androidx/androidx_annotation_annotation.ijar.jar \
+ obj/ui/gfx/geometry/mojom/mojom_java.turbine.jar \
+ gen/services/shape_detection/public/mojom/mojom_java_sources.srcjar
