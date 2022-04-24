49a32bc13cf4011903ab34eca723d67dc3254fcc
RockNRoll <RockNRoll@RockStar.muzik>
2022-04-25 07:56:03 +0900

    add

--- service_worker/sw.js
+++ service_worker/sw.js
@@ -4,10 +4,10 @@ mojo.config.autoLoadMojomDeps = false;
 
 importScripts("./third_party/blink/public/mojom/file/file_utilities.mojom.js");
 
-for(let i = 0;i < 0x1000;i++){
-var file_utilities_host_ptr = new blink.mojom.FileUtilitiesHostPtr();
-Mojo.bindInterface(blink.mojom.FileUtilitiesHost.name,
+for(let i = 0;i < 0x1000;i++) {
+	var file_utilities_host_ptr = new blink.mojom.FileUtilitiesHostPtr();
+	Mojo.bindInterface(blink.mojom.FileUtilitiesHost.name,
                    mojo.makeRequest(file_utilities_host_ptr).handle, "context");
 }
 
-adsfds;
+asdf;
